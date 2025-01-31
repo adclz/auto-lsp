4fc8c97022a09e1c9ff6c5801cc1a5c8998fb3e5
adclz <clauzeladrien@gmail.com>
2025-01-31 22:49:08 +0100

    feat: add bench.sh

--- benches/django.rs
+++ benches/django.rs
@@ -1,5 +1,6 @@
 #[cfg(feature = "python")]
-use auto_lsp::python::create_python_workspace;
+use auto_lsp::python::PYTHON_PARSERS;
+use auto_lsp_core::workspace::Workspace;
 use criterion::{criterion_group, Criterion};
 use lsp_types::Url;
 
@@ -11,7 +12,8 @@ fn parse_django(c: &mut Criterion) {
     c.bench_function("parse_django", move |b| {
         b.iter(|| {
             let uri = Url::parse("file:///test.py").unwrap();
-            let workspace = create_python_workspace(uri, text.clone());
+            let workspace =
+                Workspace::from_utf8(PYTHON_PARSERS.get("python").unwrap(), uri, text.clone());
             workspace
         });
     });
