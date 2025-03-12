6d02a1faf1e34e091f5c33bcd93a34a81b199a23
adclz <clauzeladrien@gmail.com>
2025-03-12 18:14:28 +0100

    feat(server): enhance open_text_document method to add file to workspace if not present

--- src/server/capabilities/open_text_document.rs
+++ src/server/capabilities/open_text_document.rs
@@ -1,23 +1,44 @@
+use auto_lsp_core::root::Root;
 use lsp_types::DidOpenTextDocumentParams;
 
 use crate::server::session::{Session, WORKSPACE};
 
 impl Session {
     pub fn open_text_document(&mut self, params: DidOpenTextDocumentParams) -> anyhow::Result<()> {
-        let uri = &params.text_document.uri;
+        let url = &params.text_document.uri;
         let mut workspace = WORKSPACE.lock();
 
-        if workspace.roots.contains_key(uri) {
+        if workspace.roots.contains_key(url) {
             // The file is already in the root
             // We can ignore this change
             return Ok(());
         };
-        let file_path = uri
-            .to_file_path()
-            .map_err(|_| anyhow::anyhow!("Failed to read file {}", uri.to_string()))?;
 
-        let (_url, root, document) = self.file_to_root(&file_path)?;
-        workspace.roots.insert(uri.clone(), (root, document));
+        let extension = params.text_document.language_id;
+
+        let extension = match self.extensions.get(&extension) {
+            Some(extension) => extension,
+            None => {
+                return Err(anyhow::format_err!(
+                    "Extension {} is not registered",
+                    extension
+                ))
+            }
+        };
+
+        let text = (self.text_fn)(params.text_document.text.clone());
+
+        let parsers = self
+            .init_options
+            .parsers
+            .get(extension.as_str())
+            .ok_or(anyhow::format_err!("No parser available for {}", extension))?;
+
+        let (root, document) = Root::from_texter(parsers, url.clone(), text)?;
+
+        workspace.roots.insert(url.clone(), (root, document));
+        workspace.resolve_references();
+        workspace.resolve_checks();
         Ok(())
     }
 }
