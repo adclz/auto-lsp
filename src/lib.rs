60d6d5abe8a3e10f79fe651de074fa61cad9e7f6
adclz <clauzeladrien@gmail.com>
2025-04-12 11:06:37 +0200

    chore(license): add GPLv3 license header to all source files

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
+/*
+This file is part of auto-lsp.
+Copyright (C) 2025 CLAUZEL Adrien
+
+auto-lsp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
 #![allow(rustdoc::private_intra_doc_links)]
 //!<div align="center" style="margin-bottom: 50px">
 //!  <h1>Auto LSP</h1>
@@ -83,7 +101,7 @@
 //! - [StackGraphs](https://github.com/github/stack-graphs)
 //! - [airblast-dev](https://github.com/airblast-dev)'s [texter](https://github.com/airblast-dev/texter), which saved hours of headache
 
-/// LSP server (enabled with feature `lsp_server`)
+// LSP server (enabled with feature `lsp_server`)
 #[cfg(any(feature = "lsp_server", test))]
 pub mod server;
 
