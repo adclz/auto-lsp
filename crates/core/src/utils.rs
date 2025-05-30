f1d4db8c085911b6c3c52bf8c794298322134826
adclz <clauzeladrien@gmail.com>
2025-05-30 11:45:29 +0200

    doc: add doc for dispatch macros

--- crates/core/src/utils.rs
+++ crates/core/src/utils.rs
@@ -1,3 +1,18 @@
+/// Calls a method on the first node that matches any of the provided types.
+/// Returns the result of the method call.
+///
+/// # Example
+///
+/// ```rust, ignore
+/// use ast::generated::{FunctionDefinition, ClassDefinition};
+/// use auto_lsp::dispatch_once;
+///
+/// /* ... */
+/// let result = dispatch_once!(node.lower(), [
+///     FunctionDefinition => return_something(db, param),
+///     ClassDefinition => return_something(db, param)
+/// ]);
+/// ```
 #[macro_export]
 macro_rules! dispatch_once {
     ($node:expr, [$($ty:ty => $method:ident($($param:expr),*)),*]) => {
@@ -9,6 +24,21 @@ macro_rules! dispatch_once {
     };
 }
 
+/// Calls a method on any node that matches any of the provided types.
+/// Unlike dispatch_once, it will not return.
+///
+/// # Example
+///
+/// ```rust, ignore
+/// use ast::generated::{FunctionDefinition, ClassDefinition};
+/// use auto_lsp::dispatch;
+///
+/// /* ... */
+/// dispatch!(node.lower(), [
+///     FunctionDefinition => get_something(db, param),
+///     ClassDefinition => get_something(db, param)
+/// ]);
+/// ```
 #[macro_export]
 macro_rules! dispatch {
     ($node:expr, [$($ty:ty => $method:ident($($param:expr),*)),*]) => {
