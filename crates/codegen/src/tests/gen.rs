f38555c8ea8c0108346cdf79ada8a38646abefc8
adclz <clauzeladrien@gmail.com>
2025-06-01 19:21:13 +0200

    test(codegen): add tests for HTML, JavaScript, C, C#, and Haskell code generation

--- crates/codegen/src/tests/gen.rs
+++ crates/codegen/src/tests/gen.rs
@@ -6,3 +6,60 @@ fn gen_python() {
         None,
     );
 }
+
+#[test]
+fn gen_html() {
+    let _result = crate::generate(
+        tree_sitter_html::NODE_TYPES,
+        &tree_sitter_html::LANGUAGE.into(),
+        None,
+    );
+}
+
+#[test]
+fn gen_javascript() {
+    let _result = crate::generate(
+        tree_sitter_javascript::NODE_TYPES,
+        &tree_sitter_javascript::LANGUAGE.into(),
+        Some(std::collections::HashMap::from([("`", "Backtick")])),
+    );
+}
+
+#[test]
+fn gen_c() {
+    let _result = crate::generate(
+        tree_sitter_c::NODE_TYPES,
+        &tree_sitter_c::LANGUAGE.into(),
+        Some(std::collections::HashMap::from([("\n", "Newline")])),
+    );
+}
+
+#[test]
+fn gen_c_sharp() {
+    let _result = crate::generate(
+        tree_sitter_c_sharp::NODE_TYPES,
+        &tree_sitter_c_sharp::LANGUAGE.into(),
+        None,
+    );
+}
+
+#[test]
+fn gen_haskell() {
+    let _result = crate::generate(
+        tree_sitter_haskell::NODE_TYPES,
+        &tree_sitter_haskell::LANGUAGE.into(),
+        Some(std::collections::HashMap::from([
+            ("`", "Backtick"),
+            ("←", "LeftArrow"),
+            ("→", "RightArrow"),
+            ("⇒", "DoubleRightArrow"),
+            ("⊸", "RightTack"),
+            ("∀", "Forall"),
+            ("★", "Star"),
+            ("∃", "Exists"),
+            ("∷", "ColonColon"),
+            ("⟦", "LeftDoubleBracket"),
+            ("⟧", "RightDoubleBracket"),
+        ])),
+    );
+}
