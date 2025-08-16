338a1dcf984f21566ed0cc487e029ea12b74b703
adclz <clauzeladrien@gmail.com>
2025-08-17 00:11:56 +0200

    bench: add bench crate with codspeed and divan

---
+++ crates/bench/benches/build_ast.rs
@@ -0,0 +1,25 @@
+extern crate ast_python;
+extern crate auto_lsp;
+
+use ast_python::db::create_python_db;
+use auto_lsp::{
+    default::db::{tracked::get_ast, BaseDatabase},
+    lsp_types::Url,
+};
+
+static DJANGO: &'static str = include_str!("./django.py");
+static SOURCES: &'static [&'static str] = &[DJANGO];
+
+fn main() {
+    divan::main();
+}
+
+// Register a `fibonacci` function and benchmark it over multiple cases.
+#[divan::bench]
+fn build_ast() {
+    let db = create_python_db(SOURCES);
+    let file = db
+        .get_file(&Url::parse("file:///test0.py").unwrap())
+        .unwrap();
+    get_ast(&db, file);
+}
