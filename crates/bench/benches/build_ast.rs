1012fe8b20271c074f88549ee98b887b9a7808ef
adclz <clauzeladrien@gmail.com>
2025-08-17 17:06:44 +0200

    bench: add parse_ts (cst) and reparse benchmarks

--- crates/bench/benches/build_ast.rs
+++ crates/bench/benches/build_ast.rs
@@ -1,11 +1,20 @@
 extern crate ast_python;
 extern crate auto_lsp;
+extern crate divan;
 
-use ast_python::db::create_python_db;
+use ast_python::db::{create_python_db, PYTHON_PARSERS};
 use auto_lsp::{
-    default::db::{tracked::get_ast, BaseDatabase},
-    lsp_types::Url,
+    core::errors::ParseErrorAccumulator,
+    default::db::{file::File, tracked::get_ast, BaseDatabase, BaseDb, FileManager},
+    lsp_types::{
+        DidChangeTextDocumentParams, Position, Range, TextDocumentContentChangeEvent, Url,
+        VersionedTextDocumentIdentifier,
+    },
 };
+use divan::{counter::BytesCount, AllocProfiler};
+
+#[global_allocator]
+static ALLOC: AllocProfiler = AllocProfiler::system();
 
 static DJANGO: &'static str = include_str!("./django.py");
 static SOURCES: &'static [&'static str] = &[DJANGO];
@@ -15,10 +24,86 @@ fn main() {
 }
 
 #[divan::bench]
-fn build_ast() {
-    let db = create_python_db(SOURCES);
+fn parse_ts(bencher: divan::Bencher) {
+    let url = Url::parse("file:///django.py").expect("Failed to parse URL");
+    let mut db = BaseDb::default();
+    let file = File::from_string()
+        .db(&db)
+        .source(DJANGO.to_string())
+        .url(&url)
+        .parsers(
+            PYTHON_PARSERS
+                .get("python")
+                .expect("Python parser not found"),
+        )
+        .call()
+        .expect("Failed to create file");
+
+    db.add_file(file).expect("Failed to add file");
+
+    bencher
+        .counter(BytesCount::of_str(file.document(&db).as_str()))
+        .bench_local(|| get_ast(&db, file))
+}
+
+#[divan::bench]
+fn build_ast(bencher: divan::Bencher) {
+    let url = Url::parse("file:///django.py").expect("Failed to parse URL");
+    let mut db = BaseDb::default();
+    let file = File::from_string()
+        .db(&db)
+        .source(DJANGO.to_string())
+        .url(&url)
+        .parsers(
+            PYTHON_PARSERS
+                .get("python")
+                .expect("Python parser not found"),
+        )
+        .call()
+        .expect("Failed to create file");
+
+    db.add_file(file).expect("Failed to add file");
+
+    bencher
+        .counter(BytesCount::of_str(file.document(&db).as_str()))
+        .bench_local(|| get_ast(&db, file));
+
+    let errors = get_ast::accumulated::<ParseErrorAccumulator>(&db, file);
+    assert!(errors.is_empty(), "Expected no errors, found: {:?}", errors);
+}
+
+#[divan::bench]
+fn reparse(bencher: divan::Bencher) {
+    let mut db = create_python_db(SOURCES);
     let file = db
         .get_file(&Url::parse("file:///test0.py").unwrap())
         .unwrap();
-    get_ast(&db, file);
+
+    let change_event = DidChangeTextDocumentParams {
+        text_document: VersionedTextDocumentIdentifier {
+            uri: file.url(&db).clone(),
+            version: 0,
+        },
+        content_changes: vec![TextDocumentContentChangeEvent {
+            range: Some(Range {
+                start: Position {
+                    line: 2486,
+                    character: 0,
+                },
+                end: Position {
+                    line: 2486,
+                    character: 0,
+                },
+            }),
+            range_length: None,
+            text: "# Comment\n".to_string(),
+        }],
+    };
+
+    bencher
+        .counter(BytesCount::of_str(file.document(&db).as_str()))
+        .bench_local(|| file.update_edit(&mut db, &change_event));
+
+    let errors = get_ast::accumulated::<ParseErrorAccumulator>(&db, file);
+    assert!(errors.is_empty(), "Expected no errors, found: {:?}", errors);
 }
