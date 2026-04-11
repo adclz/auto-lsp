cacbd51e40682a533e470af19b2ae2f6d7c3156e
adclz <clauzeladrien@gmail.com>
2026-04-11 19:43:09 +0200

    feat(examples): add ast-json crate with MISSING nodes tests

---
+++ examples/ast-json/build.rs
@@ -0,0 +1,21 @@
+use auto_lsp_codegen::generate;
+use std::{fs, path::PathBuf};
+
+fn main() {
+    if std::env::var("AST_GEN").unwrap_or("0".to_string()) == "0" {
+        return;
+    }
+
+    let output_path = PathBuf::from("./src/generated.rs");
+
+    fs::write(
+        output_path,
+        generate(
+            tree_sitter_json::NODE_TYPES,
+            &tree_sitter_json::LANGUAGE.into(),
+            None,
+        )
+        .to_string(),
+    )
+    .unwrap();
+}
